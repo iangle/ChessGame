17253d8417a9f0fc7ac254707764b93efdecc9da
Isaac Angle <43276870+iangle@users.noreply.github.com>
2020-01-08 09:26:57 -0800

    added a main class and an abstract base class called ChessPieces
    first commit

---
+++ ChessMain.java
@@ -0,0 +1,10 @@
+/* Isaac Angle 01-08-20
+* This is the main class for the chess game that I will create.
+* it will run the play class and will be mostly used for output*/
+
+public class ChessMain {
+
+    public static void main(String[] args){
+
+    }
+}
